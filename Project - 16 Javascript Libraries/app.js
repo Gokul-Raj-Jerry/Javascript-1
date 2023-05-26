bd10c542030b0da17416a90d510972768ad74603
Kanishka Palani <105113908+KanishkaPalani@users.noreply.github.com>
2023-05-26 16:51:40 +0530

    JS Libraries

---
+++ Project - 16 Javascript Libraries/app.js
@@ -0,0 +1,12 @@
+//loadash
+
+const customers = ['Kanishka', 'Priyanka', 'Latha'];
+
+const activeCustomers = ['Kanishka', 'Priyanka'];
+
+const inactiveCustomers = _.difference(customers, activeCustomers);
+
+console.log(inactiveCustomers);
+
+//jQuery
+
